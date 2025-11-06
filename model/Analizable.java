95576dea842aeb027fb1e8084d8494f6dff1e66c
PJOS17 <orantesspabloj@gmail.com>
2025-11-06 07:52:24 -0600

    Sin carpeta

--- model/Analizable.java
+++
@@ -1,6 +0,0 @@
-package model.interfaces;
-
-public interface Analizable {
-    void generarResumenPorEtiqueta(String etiquetaNombre);
-    void generarMetricas();
-}
--- model/Transcripcion.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class Transcripcion extends Documento {
-    public Transcripcion(String titulo, String autor, String contenido){
-        super(titulo, autor, contenido);
-    }
-}
